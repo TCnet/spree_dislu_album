45f071efd650cef8b35a53ef27400abe8e3dc165
Hxmusic <hxmusic@foxmail.com>
2019-11-09 23:12:55 +0800

    change ProductsHelperDecorator back

--- app/helpers/products_helper_decorator.rb
+++ app/helpers/products_helper_decorator.rb
@@ -1,32 +1,6 @@
-# module Spree
-#  module ProductsHelperDecorator
-#   	def product_bullet_point(product)
-#       product.bullet_point.tr("\n","|").split('|').map{|x| "<p>"+x+"</p>"}.join
-#       #description = product.bullet_point.split('/').map{|x| "<p>"+x+"</p>"}.join
-#      # description = product.bullet_point.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
-#     end
-
-#     def product_description(product)
-#       description = if Spree::Config[:show_raw_product_description]
-#                       product.description
-#                     else
-#                       str=product.description.to_s.gsub(/(<br>?)/m,'<p></p>')
-#                       str.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
-
-#                      # product.description.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
-                      
-#                     end
-#       description.blank? ? Spree.t(:product_has_no_description) : description
-#     end
-#   end
-# end
-
-
-# Spree::ProductsHelper.prepend Spree::ProductsHelperDecorator
-
-Spree::ProductsHelper.class_eval do
-  module Spree::ProductsHelperDecorator
-    def product_bullet_point(product)
+module Spree
+ module ProductsHelperDecorator
+  	def product_bullet_point(product)
       product.bullet_point.to_s.tr("\n","|").split('|').map{|x| "<p>"+x+"</p>"}.join
       #description = product.bullet_point.split('/').map{|x| "<p>"+x+"</p>"}.join
      # description = product.bullet_point.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
@@ -44,11 +18,37 @@ Spree::ProductsHelper.class_eval do
                     end
       description.blank? ? Spree.t(:product_has_no_description) : description
     end
-
   end
+end
+
+
+Spree::ProductsHelper.prepend Spree::ProductsHelperDecorator
+
+# Spree::ProductsHelper.class_eval do
+#   module Spree::ProductsHelperDecorator
+#     def product_bullet_point(product)
+#       product.bullet_point.to_s.tr("\n","|").split('|').map{|x| "<p>"+x+"</p>"}.join
+#       #description = product.bullet_point.split('/').map{|x| "<p>"+x+"</p>"}.join
+#      # description = product.bullet_point.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
+#     end
+
+#     def product_description(product)
+#       description = if Spree::Config[:show_raw_product_description]
+#                       product.description
+#                     else
+#                       str=product.description.to_s.gsub(/(<br>?)/m,'<p></p>')
+#                       str.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
+
+#                      # product.description.to_s.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>')
+                      
+#                     end
+#       description.blank? ? Spree.t(:product_has_no_description) : description
+#     end
+
+#   end
   
 
-  Spree::ProductsHelper.prepend self
-end
+#   Spree::ProductsHelper.prepend self
+# end
 
  
